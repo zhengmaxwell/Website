ea2ce471745ac4f83da26796772c675da7b5aee9
zhengmaxwell <zhengmaxwell@gmail.com>
2020-09-07 14:41:44 -0400

    hello world and shared folder

---
+++ src/components/MainComponent.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Main extends Component {
+
+    render() {
+        return (
+            <div>
+                Hello World
+            </div>
+        )
+    }
+}
+
+
+export default Main;
---
+++ src/shared/experiences.js
@@ -0,0 +1,11 @@
+export const EXPERIENCES =
+[
+    {
+        id: 0,
+        company: '',
+        position: '',
+        location: '',
+        period: '',
+        description: ''
+    }
+]
