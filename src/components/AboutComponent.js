a3c364d19b8ef803b980b0366dcd9853906bdd45
zhengmaxwell <zhengmaxwell@gmail.com>
2020-09-11 19:16:43 -0400

    about added

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -7,7 +7,8 @@ export default class About extends Component {
     constructor(props) {
         super(props);
 
-        const description = 'I\'m a robotics engineering student who is passionate about sustainability.';
+        this.description = 'I\'m a robotics engineering student at the Univeristy of Toronto who is passionate about sustainability. I am interested in \
+                            the environment, energy consumption, and waste mangement';
 
         this.RenderAbout = this.RenderAbout.bind(this);
     }
@@ -19,6 +20,17 @@ export default class About extends Component {
                     <h2>About Me</h2>
                 </Row>
                 <hr/>
+                <div className="row-content">
+                    <Row className="justify-content-center">
+                        <img src="/images/random.jpg" className="rounded-circle" width="200" height="200" alt="Me"/>
+                    </Row>
+                    <Row>
+                        <Col md={{size:10, offset:1}}>
+                            <br/>
+                            {this.description}
+                        </Col>
+                    </Row>
+                </div>
             </Container>
         );
     }
