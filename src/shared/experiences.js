ec17f2c5eed0a34dcf24464e635b157ba8ea5a56
zhengmaxwell <zhengmaxwell@gmail.com>
2020-09-18 21:31:59 -0400

    experiences summaries

--- src/shared/experiences.js
+++ src/shared/experiences.js
@@ -6,23 +6,20 @@ export const EXPERIENCES =
         location: 'Kingston, Ontario',
         period: 'May 2020 - August 2020',
         icon: 'fas fa-train',
-        description: 'Supported development of Train Management System for Wuhu and Cairo Monorails. \
-                        Simulated train signals with Python using CANbus and CANopen communication protocols. \
-                        Developed WPF app with C# for tracking train alarm information. \
-                        Implemented web based REST API to send and receive train signal information. \
-                        Automated regression tests with Windows Automation technologies and Robot Framework. \
-                        Decreased development cycle time for Rail Control Solutions Team by 200% during testing',
+        description: 'My second engineering internship was seemingly at the same company as my first, however, BT shares the same parent company (Bombardier Inc.) with BA \
+                        but should not be confused as the same company. I worked with the Rail Control and Solutions Team to manage train communications on the Wuhu and Cairo Monorails. \
+                        I supported the development of the Train Management System which oversees all the signals on the train. I implemented applications to track these signals \
+                        and created automated regressions tests.'
     },
     {
-        company: 'BOMBARDIER Aerospace',
+        company: 'BOMBARDIER Aviation',
         position: 'Data Engineering Intern',
         location: 'Toronto, Ontario',
         icon: 'fas fa-plane',
         period: 'April 2019 - August 2019',
-        description: 'Supported development of Global 7500 Business Aircraft. \
-                        Increased efficiency of Change Management team by automating all daily tasks with Python. \
-                        Queried and analyzed data from enterprise databases using SQL. \
-                        Developed a new GUI to standardize company-wide internal metrics using HTML, CSS, and JS'
+        description: 'This was my very first engineering internship in university. I thoroughly enjoyed going to work in the morning and seeing many large airplanes. \
+                        I had the opportunity to work on the Global 7500 Business Aircraft with the Change Management Team. \
+                        I was responsible for automating daily tasks and analyzing data to ensure that aircraft production was on track.'
     },
     {
         company: 'Institute of Biomaterials and Biomedical Engineering',
@@ -30,6 +27,7 @@ export const EXPERIENCES =
         location: 'Toronto, Ontario',
         icon: 'fas fa-dna',
         period: 'April 2019 - August 2019',
-        description: 'Conducted various tests using PCR and Gel Electrophoresis to sythesize and analyze DNA'
+        description: 'I learned and applied wet laboratory skills under the supervision of Dr. Leo Chou. I used molecular and cellular engineering techquies to \
+                        conduct various tests using PCR and Gel Electrophoresis to sythesize and analyze DNA'
     }
 ]
