5d700e0ee311f09bb5e64d3dcbcc052d9aeec680
zhengmaxwell <zhengmaxwell@gmail.com>
2020-09-24 10:34:13 -0400

    typo

--- src/shared/experiences.js
+++ src/shared/experiences.js
@@ -6,8 +6,8 @@ export const EXPERIENCES =
         location: 'Kingston, Ontario',
         period: 'May 2020 - August 2020',
         icon: 'fas fa-train',
-        description: 'My second engineering internship was seemingly at the same company as my first, however, while BT shares the same parent company (Bombardier Inc.) with BA \
-                        but should not be confused as the same company. I worked with the <strong>Rail Control and Solutions Team</strong> to manage train communications on the <strong>Wuhu and Cairo Monorails.</strong> \
+        description: 'My second engineering internship was seemingly at the same company as my first, however, while BT shares the same parent company (Bombardier Inc.) with BA, \
+                        it should not be confused as the same company. I worked with the <strong>Rail Control and Solutions Team</strong> to manage train communications on the <strong>Wuhu and Cairo Monorails.</strong> \
                         Furthermore, I supported the development of the <strong>Train Management System</strong> which oversees all the signals on the train. Moreover, I implemented applications to track these signals \
                         and created automated regressions tests. BT has since been bought by Alstom. Let\'s hope this doesn\'t become a trend.'
     },
